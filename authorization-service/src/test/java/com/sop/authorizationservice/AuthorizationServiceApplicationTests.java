8830b773e3dd00051f589453e8c79ddf137955f7
Jeremy | Thanawat <thanawat.j2314@gmail.com>
2022-12-10 13:23:52 +0700

    feat: initial auth service

---
+++ authorization-service/src/test/java/com/sop/authorizationservice/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.authorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
