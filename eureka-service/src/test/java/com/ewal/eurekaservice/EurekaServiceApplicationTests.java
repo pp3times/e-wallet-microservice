5b69f1fbae48c30c0f114d9acb63f8f50e3bd2cd
Jeremy | Thanawat <thanawat.j2314@gmail.com>
2022-12-10 12:22:40 +0700

    feat: Initial Eureka Service

---
+++ eureka-service/src/test/java/com/ewal/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewal.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
